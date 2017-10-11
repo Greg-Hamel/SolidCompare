44428a9aaa2bd583cba0ccdb2488e9f83c55adbc
ap12770 <ap12770@GPA-A3726-21>
2017-10-11 19:03:57 -0400

    Change name of main Form

--- SolidCompare/SolidCompare/Launcher.cs
+++ SolidCompare/SolidCompare/Launcher.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SolidCompare
 {
-    public partial class Form1 : Form
+    public partial class Launcher : Form
     {
-        public Form1()
+        public Launcher()
         {
             InitializeComponent();
         }
