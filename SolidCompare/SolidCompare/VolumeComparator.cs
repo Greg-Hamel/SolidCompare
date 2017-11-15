6ebfec9da8ec2251b940f65648afb582b3bb43f7
Gregory Hamel <hamegreg@gmail.com>
2017-11-15 15:14:29 -0500

    Add ModelDoc2 requirement for Components in constructor

--- SolidCompare/SolidCompare/VolumeComparator.cs
+++ SolidCompare/SolidCompare/VolumeComparator.cs
@@ -16,19 +16,25 @@ namespace SolidCompare
         static AssemblyDoc swAsbly;
         static Component2 swComp;
         static SldWorks.SldWorks swApp = Program.swApp;
+        static string programFile = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ProgramFiles);
 
-        private object component1;
-        private object component2;
+        static ModelDoc2 component1;
+        static ModelDoc2 component2;
 
-        public VolumeComparator(object component1, object component2)
+        public VolumeComparator(ModelDoc2 comparer, ModelDoc2 comparee)
         {
-            this.component1 = component1;
-            this.component2 = component2;
+            component1 = comparer;
+            component2 = comparee;
+        }
+
+        public void Compare()
+        {
+            // This is the main method to execute the rest of the comparison.
         }
 
         static bool CreateAssembly()
         {
-            swAsbly = (AssemblyDoc)swApp.NewDocument(@"C:\Program Files\SOLIDWORKS Corp\SOLIDWORKS\data\templates\assem.asmdot", 0, 0, 0);
+            swAsbly = (AssemblyDoc)swApp.NewDocument(programFile + @"\SOLIDWORKS Corp\SOLIDWORKS\data\templates\assem.asmdot", 0, 0, 0);
 
             if (swAsbly == null)
             {
@@ -48,8 +54,26 @@ namespace SolidCompare
             /* This will insert the componant into 
              * the newly created assembly */
 
+            if (component1.GetType() == (int)swDocumentTypes_e.swDocASSEMBLY || component2.GetType() == (int)swDocumentTypes_e.swDocASSEMBLY)
+            {
+                Logger.Info("Components are of Assembly Type");
+            }
+            else if (component1.GetType() == (int)swDocumentTypes_e.swDocPART || component2.GetType() == (int)swDocumentTypes_e.swDocPART)
+            {
+                Logger.Info("Components are of Part Type");
+            }
+            else if (component1.GetType() == (int)swDocumentTypes_e.swDocDRAWING || component2.GetType() == (int)swDocumentTypes_e.swDocDRAWING)
+            {
+                Logger.Error("VolumeComparator", "InsertComponent", "Drawing Document provided, but not supported.");
+            }
+            else if (component1.GetType() != component2.GetType())
+            {
+                Logger.Error("VolumeComparator", "InsertComponent", "Two different type of document are provided.");
+            }
+
+
             // ModelDoc2 tmpComponent = (ModelDoc2)swApp.OpenDoc7();
-            swComp = (Component2)swAsbly.AddComponent5(path1, (int)swAddComponentConfigOptions_e.swAddComponentConfigOptions_CurrentSelectedConfig, "", false, "", 0, 0, 0);
+            swComp = (Component2)swAsbly.AddComponent5(path, (int)swAddComponentConfigOptions_e.swAddComponentConfigOptions_CurrentSelectedConfig, "", false, "", 0, 0, 0);
 
             if (swComp == null)
             {
