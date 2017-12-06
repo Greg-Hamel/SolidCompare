7b013b4be1de5932c40251fb14ee48b431f7b1e1
Yeena <kpyeena@yahoo.fr>
2017-12-05 23:05:57 -0500

    Program clean up and update assembly.asmdot to local machine

--- SolidCompare/SolidCompare/Program.cs
+++ SolidCompare/SolidCompare/Program.cs
@@ -1,15 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
-using Microsoft.Win32.SafeHandles;
 using SldWorks;
-using SwConst;
 using SolidCompare.Comparators;
 using SolidCompare.Entities;
 
